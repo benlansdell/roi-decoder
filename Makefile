281b20684d43488034910d5d60a5559d57a39452
benlansdell <ben.lansdell@stjude.org>
2022-09-14 23:32:32 -0500

    Many fixes to streamlit app

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 demo:
-		streamlit run streamlit_app.py ./demodata/TSeries-07062022-001_rig__d1_512_d2_512_d3_1_order_F_frames_4000_.tif ./demodata/roiscan1.csv
+		streamlit run --server.fileWatcherType none streamlit_app.py ./demodata/TSeries-07062022-001_rig__d1_512_d2_512_d3_1_order_F_frames_4000_.tif ./demodata/roiscan1.csv 
+
+#The option --server.fileWatcherType none is used to avoid the error 'OSError: [Errno 28] inotify watch limit reached'
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
-from glob import glob 
+from glob import glob
+from turtle import onclick 
 import skimage.io
 from skimage.transform import downscale_local_mean, rescale, resize
 import numpy as np
@@ -22,6 +23,7 @@ from joblib import Parallel, delayed
 from sklearn.linear_model import LogisticRegression
 
 import streamlit as st
+from directorypicker import st_directory_picker
 
 ##Now all of these things are settings in the GUI (left panel)
 
@@ -144,7 +146,7 @@ def load_image(tif_name : str) -> np.ndarray:
     print(f"{int(end_time - start_time)} seconds to load tiff stack.")
     return im
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def build_localized_decoder(tone_file, im, box_size = 4, n_frames = None,
                             scale_factor = 128):
 
@@ -207,7 +209,10 @@ def build_localized_decoder(tone_file, im, box_size = 4, n_frames = None,
     unique_tones = np.unique(tone_labels)
     unique_tones = sorted(unique_tones[unique_tones != '0.0'], key = lambda x: int(x))
 
+    prog_bar = st.sidebar.progress(0)
+
     for i in tqdm(range(n_grid_pts)):
+        prog_bar.progress(float((i+1)/n_grid_pts))
         f1_row = []
         re_row = []
         pr_row = []
@@ -274,7 +279,47 @@ def build_localized_decoder(tone_file, im, box_size = 4, n_frames = None,
 
     return f1_scores, re_scores, pr_scores, acc_scores, im, n_frames, overall_f1, grid_dim, all_tones
 
+import matplotlib
+@st.cache(hash_funcs={matplotlib.figure.Figure: hash})
+def make_plots(f1_scores, pr_scores, re_scores, im, grid_dim, box_size, all_tones, transparent, plotbounds):
+    fig_f1, _ = plot_decoder(f1_scores,
+                        im,
+                        grid_dim = grid_dim,
+                        box_size = box_size,
+                        freq_labels = all_tones,
+                        transparent = transparent,
+                        plotbounds = plotbounds)
+    fig_pr, _ = plot_decoder(pr_scores,
+                        im,
+                        grid_dim = grid_dim,
+                        box_size = box_size,
+                        freq_labels = all_tones,
+                        transparent = transparent,
+                        plotbounds = plotbounds)
+    fig_re, _ = plot_decoder(re_scores,
+                        im,
+                        grid_dim = grid_dim,
+                        box_size = box_size,
+                        freq_labels = all_tones,
+                        transparent = transparent,
+                        plotbounds = plotbounds)
+    return fig_f1, fig_pr, fig_re
+
 def st_main(args):
+
+    def plot_button():
+        os.makedirs(os.path.dirname(plot_path), exist_ok = True)
+        fn_out = f'{plot_path}_{basename}_localized_decoder_boxsize_{box_size}_nframes_{n_frame:04}_f1_score.png'
+        fig_f1.savefig(fn_out, transparent=transparent)
+        fn_out = f'{plot_path}_{basename}_localized_decoder_boxsize_{box_size}_nframes_{n_frame:04}_precision.png'
+        fig_pr.savefig(fn_out, transparent=transparent)
+        fn_out = f'{plot_path}_{basename}_localized_decoder_boxsize_{box_size}_nframes_{n_frame:04}_recall.png'
+        fig_re.savefig(fn_out, transparent=transparent)
+    
+    def results_button():
+        os.makedirs(os.path.dirname(out_path), exist_ok = True)
+        with open(out_path, 'wb') as handle:
+            pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     tone_file = args.tonefile
     tif_name = args.tifname
@@ -284,91 +329,74 @@ def st_main(args):
     n_frames_total = len(all_frames)
 
     st.sidebar.title("ROI decoder")
-    st.sidebar.write("Input data:")
-    st.sidebar.markdown("""---""")
-    st.sidebar.write("Decoder settings:")
-
     form = st.sidebar.form(key='my_form')
+    form.write("Decoder parameters")
     n_frame = form.slider("Number of frames", 0, n_frames_total, value = n_frames_total, help = 'Number of frames to use for decoding')
     scale_factor = form.slider("Scale factor", 4, 64, value = 64, step = 8, help = 'Downsample the image by this factor')
     box_size = form.slider("Box size", 1, 10, value = 4, help = 'Size of the box to use for decoding ("pixels" in downsized image)')
 
+    form.write("Visualisation parameters")
     transparent = form.checkbox("Transparent overlay", value = False, help = 'Overlay the decoded ROI on the original image')
-    custom_range = form.checkbox("Custom range", value = False, help = 'Use a custom range for the colorbar')
+    custom_range = form.checkbox("Custom color range", value = False, help = 'Use a custom range for the colorbar')
+    plot_min = form.slider('V min', 0.0, 1.0, value = 0., step = 0.1)
+    plot_max = form.slider('V max', 0., 1.0, value = 0.8, step = 0.1)
     if custom_range:
-        plot_min = form.slider('V min', 0.0, 1.0, value = 0., step = 0.1)
-        plot_max = form.slider('V max', 0., 1.0, value = 0.8, step = 0.1)
         if plot_max < plot_min: plot_max = plot_min
         plotbounds = [plot_min, plot_max]
     else:
         plotbounds = None
     submit = form.form_submit_button(label='Run')
 
-    if submit:
+    if submit or plot_button or results_button:
         f1_scores, re_scores, pr_scores, acc_scores, im, n_frame, overall_f1, grid_dim, all_tones = \
                         build_localized_decoder(tone_file, all_frames, 
                                                 box_size = box_size, n_frames = n_frame,
                                                 scale_factor = scale_factor)
+    else: 
+        #Return generate empty responses 
+        f1_scores = np.zeros((1,1,1))
+        re_scores = np.zeros((1,1,1))
+        pr_scores = np.zeros((1,1,1))
+        acc_scores = np.zeros((1,1,1))
+        im = np.zeros((1,1,1))
+        n_frame = 1
+        overall_f1 = np.zeros((1,1))
+        grid_dim = 1
+        all_tones = [0]
 
-        basename = os.path.basename(tif_name).split('.')[0]
+    basename = os.path.basename(tif_name).split('.')[0]
 
-        fig_f1, _ = plot_decoder(f1_scores,
-                            im,
-                            grid_dim = grid_dim,
-                            box_size = box_size,
-                            freq_labels = all_tones[1:],
-                            transparent = transparent,
-                            plotbounds = plotbounds)
+    fig_f1, fig_pr, fig_re = make_plots(f1_scores, pr_scores, re_scores, im, grid_dim, box_size, all_tones[1:], transparent, plotbounds)
+
+    with st.expander("Input", expanded = True):
+        input_form = st.form(key='inputform')
+        st_directory_picker(base = input_form)
+        load_d = input_form.form_submit_button(label='Load')
+
+    with st.expander("F1", expanded = True):
         st.markdown('<div style="text-align: center;">F1 scores</div>', unsafe_allow_html=True)
         st.write(fig_f1)
 
-        fig_pr, _ = plot_decoder(pr_scores,
-                            im,
-                            grid_dim = grid_dim,
-                            box_size = box_size,
-                            freq_labels = all_tones[1:],
-                            transparent = transparent,
-                            plotbounds = plotbounds)
+    with st.expander("Precision"):
         st.markdown('<div style="text-align: center;">Precision scores</div>', unsafe_allow_html=True)
         st.write(fig_pr)
 
-        fig_re, _ = plot_decoder(re_scores,
-                            im,
-                            grid_dim = grid_dim,
-                            box_size = box_size,
-                            freq_labels = all_tones[1:],
-                            transparent = transparent,
-                            plotbounds = plotbounds)
+    with st.expander("Recall"):
         st.markdown('<div style="text-align: center;">Recall scores</div>', unsafe_allow_html=True)
         st.write(fig_re)
 
-        end_time = time.time()
+    with st.expander("Output"):
+        plot_path = st.text_input('Plot path', value = './roi_decoding_results', help = 'Directory to save the plots')
+        out_path = st.text_input('Output file', value = './results.pkl', help = 'Path to save the results')
+        st.button('Save plots', on_click= plot_button)
+        st.button('Save results', on_click = results_button)
 
-        results = {'n_frame': n_frame, 'box_size': box_size, 'f1': f1_scores, 're': re_scores, 'pr': pr_scores, 'acc': acc_scores,
-                'overall_f1': overall_f1}
+    end_time = time.time()
 
-        print(f"Total time: {int(end_time-start_time)} seconds. Decoded {n_frame} frames. Overall F1 scores: {overall_f1}")
+    results = {'n_frame': n_frame, 'box_size': box_size, 'f1': f1_scores, 're': re_scores, 'pr': pr_scores, 'acc': acc_scores,
+            'overall_f1': overall_f1}
 
-    st.sidebar.markdown("""---""")
-    st.sidebar.write("Output:")
-
-    plot_path = st.sidebar.text_input('Plot path', value = './roi_decoding_results', help = 'Directory to save the plots')
-    out_path = st.sidebar.text_input('Output file', value = './results.pkl', help = 'Path to save the results')
-
-    if st.sidebar.button('Save plots') and submit:
-        os.makedirs(os.path.dirname(plot_path), exist_ok = True)
-        fn_out = f'{plot_path}_{basename}_localized_decoder_boxsize_{box_size}_nframes_{n_frame:04}_f1_score.png'
-        fig_f1.savefig(fn_out, transparent=transparent)
-        fn_out = f'{plot_path}_{basename}_localized_decoder_boxsize_{box_size}_nframes_{n_frame:04}_precision.png'
-        fig_pr.savefig(fn_out, transparent=transparent)
-        fn_out = f'{plot_path}_{basename}_localized_decoder_boxsize_{box_size}_nframes_{n_frame:04}_recall.png'
-        fig_re.savefig(fn_out, transparent=transparent)
-    
-    
-    if st.sidebar.button('Save results') and submit:
-        os.makedirs(os.path.dirname(out_path), exist_ok = True)
-        with open(out_path, 'wb') as handle:
-            pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print(f"Total time: {int(end_time-start_time)} seconds. Decoded {n_frame} frames. Overall F1 scores: {overall_f1}")
 
 
 if __name__ == '__main__':
